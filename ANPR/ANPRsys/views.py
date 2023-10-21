dbb937b921fca5c454d0f67ff20a504769063d0e
acepriority <sudaicejeymack@gmail.com>
2023-10-21 14:26:34 +0300

    Inital Commit

--- ANPR/ANPRsys/views.py
+++ ANPR/ANPRsys/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from components.otp_generator import generate_otp
-from components.temp_password_generator import temporary_password
+# from components.otp_generator import generate_otp
+# from components.temp_password_generator import temporary_password
 from django.http import HttpResponse
 
 
-"""def home(request):
-    return render(request, 'otpemail.html')"""
-
-
 def home(request):
+    return render(request, 'otpemail.html')
+
+
+"""def home(request):
     temporary_password()
-    return HttpResponse('<h2>otp sent</h2>')
+    return HttpResponse('<h2>otp sent</h2>')"""
