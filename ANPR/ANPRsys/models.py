419998fcf1c8c6a2eb7cde8d9f0677da9d4713f1
MKBenjie <mugaggabenjie@gmail.com>
2023-10-26 18:48:24 +0300

    gitignore file

--- ANPR/ANPRsys/models.py
+++ ANPR/ANPRsys/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Custom fields
